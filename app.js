00c9cf8281a19cb47f4668b3f08d3d3d1efa1c47
eugenepokalyuk <eugene.pokalyuk@gmail.com>
2023-03-24 12:32:31 +0700

    1

--- app.js
+++ app.js
@@ -96,27 +96,103 @@ bot.on('message', (msg) => {
     }
 });
 
+// bot.on('callback_query', (query) => {
+// const chatId = query.message.chat.id;
+// const messageId = query.message.message_id;
+// const data = query.data.split('_');
+// const action = data[0];
+// const hatId = data[1];
+// if (action === 'prev') {
+//     // Обработчик для перехода к предыдущей странице
+// } else if (action === 'next') {
+//     // Обработчик для перехода к следующей странице
+// } else if (action === 'buy') {
+//     // Обработчик для добавления товара в корзину
+// } else if (action === 'back') {
+//     bot.editMessageText('Что вы хотели бы сделать?', { chat_id: chatId, message_id: messageId, reply_markup: mainMenu });
+// }
+// });
+
 bot.on('callback_query', (query) => {
-const chatId = query.message.chat.id;
-const messageId = query.message.message_id;
-const data = query.data.split('_');
-const action = data[0];
-const hatId = data[1];
-if (action === 'prev') {
-    // Обработчик для перехода к предыдущей странице
-} else if (action === 'next') {
-    // Обработчик для перехода к следующей странице
-} else if (action === 'buy') {
-    // Обработчик для добавления товара в корзину
-} else if (action === 'back') {
-    bot.editMessageText('Что вы хотели бы сделать?', { chat_id: chatId, message_id: messageId, reply_markup: mainMenu });
-}
+    const chatId = query.message.chat.id;
+    const messageId = query.message.message_id;
+    const data = query.data.split('_');
+    const action = data[0];
+    const hatId = data[1];
+    if (action === 'prev') {
+        const currentHatIndex = currentHats.findIndex((hat) => hat.id === hatId);
+        const prevHat = currentHats[currentHatIndex - 1];
+        if (prevHat) {
+            const prevHatId = prevHat.id;
+            const prevHatDesc = `Время года: ${prevHat.season}\nЦвет: ${prevHat.color}\nРазмер: ${prevHat.size}`;
+            bot.editMessageMedia(
+                {
+                    type: 'photo',
+                    media: { source: prevHat.imageUrl },
+                },
+                {
+                    chat_id: chatId,
+                    message_id: messageId,
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                { text: '←', callback_data: `prev_${prevHatId}` },
+                                { text: `${currentHatIndex + 1}/${currentHats.length}`, callback_data: 'noop' },
+                                { text: '→', callback_data: `next_${prevHatId}` },
+                            ],
+                            [{ text: prevHat.season, callback_data: 'noop' }],
+                            [{ text: prevHatDesc, callback_data: 'noop' }],
+                            [{ text: 'Добавить в корзину', callback_data: `buy_${prevHatId}` }],
+                            [{ text: 'Назад', callback_data: 'back' }],
+                        ],
+                    },
+                }
+            );
+        }
+    } else if (action === 'next') {
+        const currentHatIndex = currentHats.findIndex((hat) => hat.id === hatId);
+        const nextHat = currentHats[currentHatIndex + 1];
+        if (nextHat) {
+            const nextHatId = nextHat.id;
+            const nextHatDesc = `Время года: ${nextHat.season}\nЦвет: ${nextHat.color}\nРазмер: ${nextHat.size}`;
+            bot.editMessageMedia(
+                {
+                    type: 'photo',
+                    media: { source: nextHat.imageUrl },
+                },
+                {
+                    chat_id: chatId,
+                    message_id: messageId,
+                    reply_markup: {
+                        inline_keyboard: [
+                            [
+                                { text: '←', callback_data: `prev_${nextHatId}` },
+                                { text: `${currentHatIndex + 2}/${currentHats.length}`, callback_data: 'noop' },
+                                { text: '→', callback_data: `next_${nextHatId}` },
+                            ],
+                            [{ text: nextHat.season, callback_data: 'noop' }],
+                            [{ text: nextHatDesc, callback_data: 'noop' }],
+                            [{ text: 'Добавить в корзину', callback_data: `buy_${nextHatId}` }],
+                            [{ text: 'Назад', callback_data: 'back' }],
+                        ],
+                    },
+                }
+            );
+        }
+    } else if (action === 'buy') {
+        const hat = hats.find((hat) => hat.id === hatId);
+        cart.push(hat);
+        bot.answerCallbackQuery(query.id, { text: `Товар "${hat.name}" добавлен в корзину` });
+    } else if (action === 'back') {
+        bot.editMessageText('Что вы хотели бы сделать?', { chat_id: chatId, message_id: messageId, reply_markup: mainMenu });
+    }
 });
 
+
 db.serialize(() => {
-db.run( `CREATE TABLE IF NOT EXISTS hats ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, image_url TEXT NOT NULL, season TEXT NOT NULL, color TEXT NOT NULL, size TEXT NOT NULL, price INTEGER NOT NULL )` );
-// Добавьте здесь свои товары в базу данных
-db.run(`
+    db.run(`CREATE TABLE IF NOT EXISTS hats ( id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, image_url TEXT NOT NULL, season TEXT NOT NULL, color TEXT NOT NULL, size TEXT NOT NULL, price INTEGER NOT NULL )`);
+    // Добавьте здесь свои товары в базу данных
+    db.run(`
     INSERT INTO hats (name, image_url, season, color, size, price)
     VALUES ('Шапка зимняя', 'https://example.com/winter-hat.jpg', 'Зима', 'Черный', 'M', 1000)
 `);
